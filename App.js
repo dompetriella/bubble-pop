9faf2f7c44b8ec85a4b1fe1e9ec419bad46ed628
Dominic Petriella <50121548+dompetriella@users.noreply.github.com>
2022-01-29 23:36:54 -0500

    added start screen, button functionality and silly

--- App.js
+++ App.js
@@ -1,9 +1,25 @@
 import { useState, useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableWithoutFeedback, View } from 'react-native';
+import { Button, TouchableOpacity } from 'react-native-web';
 
 export default function App() {
 
     const [mainContent, setMainContent] = useState([])
+    const phrases = [
+        "Hey, you know that bubble had a family",
+        "You happy now? No more bubble.  Game's over.  Go home.",
+        "oh my god, you killed him",
+        "Bubble's dead now. Bet you're real proud of yourself champ",
+        "Bubble popped. Now go outside and play",
+        "BUBBLE BUDDY NO!!!!",
+        "You really blew it, now you have no bubble",
+        "BUBBLE DELETED",
+        "Gone before his time",
+        "What, do you hate bubbles or something?",
+        "You did this."
+    ]
+
+    let copyPhrases = [...phrases]
 
     const StartScreen = () => {
         setMainContent(
@@ -11,9 +27,51 @@ export default function App() {
                 <View style={styles.titleContainer}>
                     <Text style={styles.title}>Bubble Pop</Text>
                 </View>
-                <View style={styles.startButton}>
+                <TouchableOpacity style={styles.startButton} onPress={renderBubbleScreen}>
                     <Text style={styles.startButtonText}>Start Game</Text>
-                </View>
+                </TouchableOpacity>
+
+            </View>
+        )
+    }
+
+
+    const renderBubbleScreen = () => {
+        setMainContent(
+            <View style={styles.container}>
+                <TouchableOpacity onPress={bubblePop}>
+                    <Bubble></Bubble>
+                </TouchableOpacity>
+            </View>
+
+        )
+    }
+
+    const bubblePop = () => {
+        let selectedPhrase = ""
+
+        if (copyPhrases.length > 0) {
+            let randomIndex = Math.floor(Math.random()*copyPhrases.length)
+            selectedPhrase = copyPhrases[randomIndex]
+            copyPhrases.splice(randomIndex, 1)
+        }
+
+        else {
+            copyPhrases = [...phrases]
+            let randomIndex = Math.floor(Math.random()*copyPhrases.length)
+            selectedPhrase = copyPhrases[randomIndex]
+            copyPhrases.splice(randomIndex, 1)
+        }
+
+        setMainContent(
+            <View style={styles.container}>
+                <TouchableOpacity onPress={bubblePop}>
+                    <Text style={{width: '75%', alignSelf: 'center', textAlign: 'center', fontSize: 20}}>{selectedPhrase}</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.anotherBubble} onPress={renderBubbleScreen}>
+                    <Text style={{fontSize: 15, color: 'white'}}>Get another bubble</Text>
+                </TouchableOpacity>
+                
             </View>
         )
     }
@@ -114,6 +172,16 @@ const styles = StyleSheet.create({
         fontSize: 25,
         fontWeight: 'bold',
         color: 'black'
+    },
+
+    anotherBubble: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 50,
+        width: '50%',
+        marginTop: 50,
+        borderRadius: 20,
+        backgroundColor: 'purple'
     }
 
 });
